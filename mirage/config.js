8577226b24a1e43fdc214d92faef9de7b80f3c7c
William Grant <willmgrant@gmail.com>
2017-12-15 10:46:30 +0200

    added missing rental descriptions and now linked to specifc rentals

--- mirage/config.js
+++ mirage/config.js
@@ -11,7 +11,8 @@ export default function() {
         city: 'San Francisco',
         "category": 'Estate',
         bedrooms: 15,
-        image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg'
+        image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
+        description: "This grand old mansion sits on over 100 acres of rolling hills and dense redwood forests."
       }
     }, {
       type: 'rentals',
@@ -22,7 +23,8 @@ export default function() {
         city: 'Seattle',
         "category": 'Condo',
         bedrooms: 1,
-        image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg'
+        image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg',
+        description: "A commuters dream. This rental is within walking distance of 2 bus stops and the Metro."
       }
     }, {
       type: 'rentals',
@@ -33,7 +35,8 @@ export default function() {
         city: 'Portland',
         "category": 'Apartment',
         bedrooms: 3,
-        image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg'
+        image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg',
+        description: "Convenience is at your doorstep with this charming downtown rental. Great restaurants and active night life are within a few feet."
       }
     }];
 
